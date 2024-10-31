dfcbeb2ae414bb2f767ae5b16acc7bcc2a8e22f0
enoch <15328504005@163.com>
2024-10-31 15:13:53 +0800

    初始项目

---
+++ src/main/java/com/suinian/qinghebackendproj/QingheBackendProjApplication.java
@@ -0,0 +1,13 @@
+package com.suinian.qinghebackendproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingheBackendProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingheBackendProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suinian/qinghebackendproj/QingheBackendProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suinian.qinghebackendproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingheBackendProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
